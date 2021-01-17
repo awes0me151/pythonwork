542aee1ca6c95b7f56c6c2dc738e4cb682800ce2
Justin Lau <justinlau73@gmail.com>
2021-01-17 14:51:19 -0500

    updated game to prevent cheating, made game more interactive

--- guessingame.py
+++ guessingame.py
@@ -16,30 +16,55 @@ def test_positive(number):
 def coin_flip(guess, bet):
 	if RepresentsInt(bet) and test_positive(bet):
 		global money
-        num = random.randint(1,2)
-        if int(bet) > money:
-			print("You cannot do this! Insufficient funds.")
+		num = random.randint(1,2)
+		if int(guess) == num:
+			money += int(bet)
+			print("You guessed correctly. " + "You now have " + str(money) + ".")
 		else:
-			if int(guess) == num:
-            	money += int(bet)
-				print("You guessed correctly. " + "You now have " + str(money) + ".")
-  			else:
-				money -= int(bet)
-                print("You guessed incorrectly. " + "You now have " + str(money)+ ".")
+			money -= int(bet)
+			print("You guessed incorrectly. " + "You now have " + str(money)+ ".")
 	else:
-		print ("Try again")
+		print ("Wait a second, you made a wrong bet. Try again please.")
 
-quit = False
-while not quit:
-	print("You have " + str(money) + ".")
-    if money == 0:
-      print("Game over!")
-      quit = True
-    else:
-      choice = input('Would you like to continue? (Y/N) ')
-      if choice.upper().strip() == 'Y' or choice.upper().strip() == 'YES':
-          bet_input = input('How much money would you like to bet? ')
-          guess_input = input('What side would you like to guess? (1/2) ')
-          coin_flip(guess_input, bet_input)
-      else:
-          quit = True
+print("Welcome to my game! Starting with 700 dollars, you bet your money on either 1 or 2. \nIf you guess correctly, you win the money you bet, and if you are incorrect, you lose the money you bet! \nCheating is not allowed!")
+print("You have " + str(money) + ".")
+while True:
+	if money == 0:
+		exit("Game over!")
+	else:
+		choice = input('Would you like to continue? (Y/N) ')
+		if choice.upper().strip() == 'Y' or choice.upper().strip() == 'YES':
+			bet_input = input('How much money would you like to bet? ')
+			bad_input = not bet_input.isdigit() or int(bet_input) > money or int(bet_input) <= 0
+			if bad_input:
+				print ("You're cheating! I'm giving you one more chance.")
+				bet_input = input('So, how much will you bet this time, cheater? ')
+				bad_input = not bet_input.isdigit() or int(bet_input) > money or int(bet_input) <= 0
+				if bad_input:
+					exit("Game over, cheater!")
+				else:
+					bet_input = int(bet_input)
+					guess_input = input('What side would you like to guess? (1/2) ')
+					if guess_input == '1' or guess_input == '2':
+						coin_flip(guess_input, bet_input)
+					else:
+						print("Choose either 1 or 2.")
+						guess_input = input('What side would you like to guess? (1/2) ')
+						if guess_input == '1' or guess_input == '2':
+							coin_flip(guess_input, bet_input)
+						else:
+							exit("Since you can't follow rules, this game isn't for you!")
+			else:
+				bet_input = int(bet_input)
+				guess_input = input('What side would you like to guess? (1/2) ')
+				if guess_input == '1' or guess_input == '2':
+					coin_flip(guess_input, bet_input)
+				else:
+					print ("Choose either 1 or 2.")
+					guess_input = input('What side would you like to guess? (1/2)')
+					if guess_input == '1' or guess_input == '2':
+						coin_flip(guess_input, bet_input)
+					else:
+						exit("Since you can't follow rules, this game isn't for you!")
+		else:
+			exit("Goodbye! You ended the game with " + str(money) + " dollars.")
